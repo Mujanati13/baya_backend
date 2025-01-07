e779a37ca88ac7e02afdf5d4cf1351c4ffbfd5c0
mujanati13 <simojanati.1j@gmail.com>
2025-01-07 12:42:51 +0100

    commit

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: "51.38.99.75",
-    user: "root",
-    password: "PASSWORD",
-    port: "3306",
-    database: "bayashop",
-    waitForConnections: true,
-    connectionLimit: 10, 
-    queueLimit: 0,
+  host: "84.247.166.36",
+  user: "root",
+  password: "simo1234",
+  database: "bayashop",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // const pool = mysql.createPool({
@@ -23,26 +22,26 @@ const pool = mysql.createPool({
 // });
 
 // Handle connection errors
-pool.on('connection', (connection) => {
-    console.log("New MySQL connection established.");
+pool.on("connection", (connection) => {
+  console.log("New MySQL connection established.");
 });
 
-pool.on('acquire', (connection) => {
-    console.log(`Connection ${connection.threadId} acquired.`);
+pool.on("acquire", (connection) => {
+  console.log(`Connection ${connection.threadId} acquired.`);
 });
 
-pool.on('release', (connection) => {
-    console.log(`Connection ${connection.threadId} released.`);
+pool.on("release", (connection) => {
+  console.log(`Connection ${connection.threadId} released.`);
 });
 
 // Function to test database connectivity
 pool.getConnection((err, connection) => {
-    if (err) {
-        console.error("Error connecting to the database:", err.message);
-    } else {
-        console.log("Connected to the MySQL database using connection pool.");
-        connection.release(); // Release the connection back to the pool
-    }
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  } else {
+    console.log("Connected to the MySQL database using connection pool.");
+    connection.release(); // Release the connection back to the pool
+  }
 });
 
 module.exports = pool;
